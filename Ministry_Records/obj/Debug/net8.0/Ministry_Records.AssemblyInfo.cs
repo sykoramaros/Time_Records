eb55cb7999ef912e4c7d9ddc22cb505902209a84
Marosh Sykora <sykoramaros@gmail.com>
2025-01-26 17:22:17 +0100

    Uspesne propojeni s externi MonsterAsp MariaDB databazi a provedena prvni migrace a update na databazi AppUser.cs tridu

--- Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
+++ Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b3558d9c69cdfe698643e222db1a671ebd6271")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b662ab3d54ce90809acc03492e571a17cfe5053c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
