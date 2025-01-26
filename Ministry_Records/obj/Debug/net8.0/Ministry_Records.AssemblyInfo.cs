7f844293616f353e3a7c2d75564b4a9c02789ff5
Marosh Sykora <sykoramaros@gmail.com>
2025-01-26 18:21:59 +0100

    Vytvoreny model Record a jeho dto a provedena migrace a upload na databazi

--- Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
+++ Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b662ab3d54ce90809acc03492e571a17cfe5053c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb55cb7999ef912e4c7d9ddc22cb505902209a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
