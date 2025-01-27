a47e343b80abcd6b46911f5a109c671f1f2a10f6
Marosh Sykora <sykoramaros@gmail.com>
2025-01-27 15:35:16 +0100

    Do RrecordService pridany metody Create Edit a Delete a do RecordsController pridana funkcni metoda CreateRecord (odzkouseno na swaggeru)

--- Ministry_Records/Controllers/RecordsController.cs
+++ Ministry_Records/Controllers/RecordsController.cs
@@ -13,7 +13,7 @@ public class RecordsController : ControllerBase {
         this.recordService = recordService;
     }
     
-    [HttpGet]
+    [HttpGet("getAllRecords")]
     public ActionResult<IEnumerable<RecordDto>> GetAllRecords() {
         var records = recordService.GetAllRecords();
         if (records == null || !records.Any()) {
@@ -22,7 +22,7 @@ public class RecordsController : ControllerBase {
         return Ok(records);
     }
     
-    [HttpGet("{id}")]
+    [HttpGet("getById/{id}")]
     public async Task<ActionResult<RecordDto>> GetRecordByIdAsync(int id) {
         var record = await recordService.GetRecordByIdAsync(id);
         if (record == null) {
@@ -30,4 +30,61 @@ public class RecordsController : ControllerBase {
         }
         return Ok(record);
     }
+    
+    // spravny json format
+    // {
+    //     "id": 0,
+    //     "date": "2023-04-15",
+    //     "recordTime": "05:40:00",
+    //     "recordStudy": 1,
+    //     "description": "zkouska swagger s pocatecni nulou"
+    // }
+    
+    [HttpPost("createRecord")]
+    public async Task<ActionResult> CreateRecordAsync([FromBody] RecordDto recordDto) {
+        if (!ModelState.IsValid) {
+            return BadRequest(ModelState);
+        }
+        try {
+            await recordService.CreateRecordAsync(recordDto);
+            return Ok();
+        }
+        catch (Exception exception) {
+            return StatusCode(500, "An error occurred while creating the record: " + exception.Message);
+        }
+    }
+    
+    // zatim haze error 400
+    [HttpPut("EditRecordByDate/{date}")]
+    public async Task<ActionResult> EditRecordByDateAsync(DateOnly date, RecordDto recordDto) {
+        await recordService.EditRecordByDateAsync(date, recordDto);
+        return Ok();
+    }
+    
+    // [HttpPut("EditRecordById/{id}")]
+    // public async Task<ActionResult> EditRecordByIdAsync(int id, RecordDto recordDto) {
+    //     await recordService.EditRecordByIdAsync(id, recordDto);
+    //     return Ok();
+    // }
+    
+    // jeste nefunguje i kdyz vraci kod 200
+    [HttpPut("EditRecordById/{id}")]
+    public async Task<IActionResult> EditRecordByIdAsync(int id, [FromBody] RecordDto editedRecord) {
+        var recordToEdit = await recordService.GetRecordByIdAsync(id);
+        if (recordToEdit == null) {
+            return NotFound("Record not found");
+        }
+        recordToEdit.Date = editedRecord.Date;
+        recordToEdit.RecordTime = editedRecord.RecordTime;
+        recordToEdit.RecordStudy = editedRecord.RecordStudy;
+        recordToEdit.Description = editedRecord.Description;
+        if (!ModelState.IsValid) {
+            return BadRequest(error: ModelState);
+        }
+        // await recordService.EditRecordByIdAsync(id, editedRecord);
+        return Ok();
+    }
+    
+    
+    
 }
--- Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
+++ Ministry_Records/obj/Debug/net8.0/Ministry_Records.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4ec8418d17a082162d78e465ca50d00984bb25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06ab59c235531b9c2b60ffa6be0639ae5729224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ministry_Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
