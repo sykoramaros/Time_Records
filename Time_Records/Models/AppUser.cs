05fa3a2030e2b81f1f13656f48bdaaa5fca806d8
Marosh Sykora <sykoramaros@gmail.com>
2025-03-04 16:45:47 +0100

    AppUser dedi z IdentityUser Guid a IdentityUserId je Guid namisto string

--- Time_Records/Models/AppUser.cs
+++ Time_Records/Models/AppUser.cs
@@ -5,6 +5,7 @@ namespace Time_Records.Models;
 
 public class AppUser : IdentityUser<Guid> { // dedi z IdentityUser spoustu vlastnosti
     
+    [MaxLength(200)]
     public string? GoogleToken { get; set; }
     
     [Range(1, int.MaxValue, ErrorMessage = "MonthTimeGoal must be at least 1")]
